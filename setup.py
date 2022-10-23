13997cad4e10c6ebcef75daf86a494e04fcfc85a
adamthomas-mitchell <atmitchell017@gmail.com>
2022-10-23 17:21:21 +0100

    Updated setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='uqlab',
     version='0.1.0',
-    description='A package for uncertainty quantification and post-hoc calibration',
-    url='',
+    url="https://github.com/AdamThomas-Mitchell/uqlab",
     author='Adam Thomas-Mitchell',
     author_email='atmitchell017@gmail.com',
-    license='MIT',
-    packages=['uqlab'],
-    zip_safe=False
+    description='A package for uncertainty quantification and post-hoc calibration',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
